8850d05a9d518a03829cc78794796be7f4602df9
daw-T <44907469+daw-T@users.noreply.github.com>
2020-02-06 21:03:55 +0300

    убирает магические числа, добавляет функцию создания фрагмента, перемешивание массива объектов

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var names = ['Артем', 'Ольга', 'Анна', 'Игорь', 'Сергей', 'Тимур', 'Elen', 'Ko4erga', 'Master', 'Matt', 'Joe'];
-var messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var NUMBER_OF_OBJECTS = 25;
+var ARR_MESSAGES_AND_NAMES = 2;
+var NAMES = ['Артем', 'Ольга', 'Анна', 'Игорь', 'Сергей', 'Тимур', 'Elen', 'Ko4erga', 'Master', 'Matt', 'Joe'];
+var MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var arrayOfNumbers;
 
 var randomArrayElement = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -21,42 +24,67 @@ var getComment = function (arrMessages, arrNames) {
 
 var getArrayOfObjectsComments = function () {
   var arrayOfComments = [];
-  for (var i = 0; i < 2; i++) {
-    arrayOfComments[i] = getComment(messages, names);
+  for (var i = 0; i < ARR_MESSAGES_AND_NAMES; i++) {
+    arrayOfComments[i] = getComment(MESSAGES, NAMES);
   }
   return arrayOfComments;
 };
 
-var getArrayOfObjectsDescription = function (b) {
-  return 'описание фотографии ' + b;
+var getMixedArrayOfNumbers = function () {
+  if (arrayOfNumbers === undefined) {
+    arrayOfNumbers = [];
+    for (var m = 0; m < NUMBER_OF_OBJECTS; m++) {
+      arrayOfNumbers[m] = m + 1;
+    }
+
+    arrayOfNumbers.sort(function () {
+      return 0.5 - Math.random();
+    });
+  }
+
+  return arrayOfNumbers;
+};
+
+var getRandomNumberPicture = function (a) {
+  return getMixedArrayOfNumbers()[a - 1];
+};
+
+var getArrayOfObjectsDescription = function (a) {
+  return 'описание фотографии ' + getMixedArrayOfNumbers()[a - 1];
 };
 
 var getObject = function (a) {
   return {
-    'url': 'photos/' + a + '.jpg',
+    'url': 'photos/' + getRandomNumberPicture(a) + '.jpg',
     'description': getArrayOfObjectsDescription(a),
     'likes': randomNumber(15, 200),
     'comments': getArrayOfObjectsComments(),
   };
 };
 
-var changePropertiesPicture = function (item, pic) {
-  item.querySelector('.picture__img').setAttribute('src', pic.url);
-  item.querySelector('.picture__comments').textContent = pic.comments.length;
-  item.querySelector('.picture__likes').textContent = pic.likes;
+var getPictureElement = function (pic) {
+  var pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').setAttribute('src', pic.url);
+  pictureElement.querySelector('.picture__comments').textContent = pic.comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = pic.likes;
+
+  return pictureElement;
 };
 
 var pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-var fragment = document.createDocumentFragment();
-for (var j = 0; j < 25; j++) {
-  var pictureElement = pictureTemplate.cloneNode(true);
+var getFragment = function (number) {
+  var fragment = document.createDocumentFragment();
+  for (var j = 0; j < number; j++) {
 
-  changePropertiesPicture(pictureElement, getObject(j + 1));
-  fragment.appendChild(pictureElement);
-}
+    fragment.appendChild(getPictureElement(getObject(j + 1)));
+  }
+
+  return fragment;
+};
 
 var pictures = document.querySelector('.pictures');
-pictures.appendChild(fragment);
+pictures.appendChild(getFragment(NUMBER_OF_OBJECTS));
